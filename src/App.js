58c2b8212850db1326cff40e26947eccff71bca3
Obinna Okwuolisa <o.okwuolisa@dickblick.com>
2023-08-23 23:00:59 +0100

    vite bundler

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
@@ -24,6 +27,6 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
 // ReactDOM.render(React.createElement(App), container);
