44077a59769df0b268095d8262e9233264babf61
Obinna Okwuolisa <o.okwuolisa@dickblick.com>
2023-08-24 16:02:23 +0100

    react query

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
+import FetchPet from "./FetchPet";
 
 const Details = () => {
-    const {id} = useParams()
-  return <h2>{id}</h2>;
+  const { id } = useParams();
+  const results = useQuery(["details", id], FetchPet);
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
 };
 export default Details;
