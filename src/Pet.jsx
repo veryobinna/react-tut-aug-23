ddd33ef7651a90fa7a8975f76c9ee3d143553dbc
Obinna Okwuolisa <o.okwuolisa@dickblick.com>
2023-08-24 14:04:45 +0100

    react router

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,11 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Pet = (props) => {
+  const { name, animal, breed, id, images, location } = props;
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+
+  if (images.length) {
+    hero = images[0];
+  }
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h1>{props.breed}</h1>
-    </div>
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </Link>
   );
 };
 
-export default Pet
+export default Pet;
